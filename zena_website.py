eb4a5818f5fef779bc19e5f002afe28725c581ac
Jaimin Patel <134231362+erjaiminp@users.noreply.github.com>
2023-07-08 15:03:49 +0400

    Update zena_website.py

--- zena_website.py
+++ zena_website.py
@@ -8,7 +8,8 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
-streamlit.write(df)
+color_list = df[0].values.tolist()
+streamlit.write(color_list)
 # put the first column into a list
 
 
