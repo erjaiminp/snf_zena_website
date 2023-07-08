0c2e32061bef8c3e4d59daa3e1a12038c1a97ac4
Jaimin Patel <134231362+erjaiminp@users.noreply.github.com>
2023-07-08 15:06:46 +0400

    Update zena_website.py

--- zena_website.py
+++ zena_website.py
@@ -9,8 +9,8 @@ my_catalog = my_cur.fetchall()
 df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
 color_list = df[0].values.tolist()
-streamlit.write(list(color_list))
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+#streamlit.write()
+option = streamlit.selectbox('Pick a sweatsuit color or style:', color_list)
 
 
 
